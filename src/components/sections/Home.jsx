e6f78709ae98c576e47b1b3707359c237d1f449b
Tudor Gradinaru <tudorgradinaru@192.168.1.114>
2025-09-20 11:28:42 +0300

    fix

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -34,11 +34,11 @@ export const Home = () => {
             My expertise lies in developing full-stack web applications using
             modern JavaScript frameworks and libraries.
           </p>
-          <div className="flex justify-center space-x-4">
+          <div className="flex items-center justify-center flex-wrap gap-x-4 gap-y-10">
             <Magnetic>
               <a
                 href="#projects"
-                className="bg-blue-200 text-black py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+                className="bg-blue-200 text-black py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] text-nowrap"
               >
                 View Projects
               </a>
@@ -46,7 +46,7 @@ export const Home = () => {
             <Magnetic>
               <a
                 href="#contact"
-                className="border border-blue-200 text-blue-200 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+                className="border border-blue-200 text-blue-200 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10 text-nowrap"
               >
                 Contact Me
               </a>
